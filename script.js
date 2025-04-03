15e5bda9d5c91d37b1353132d35e3e264bbde805
just-an-average-coder <bbblackknight640@gmail.com>
2025-04-03 20:49:46 +0530

    Update script.js
    Changed the name of the script.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Timer Speed up
+// @name         Just2earn Bypass
 // @namespace    http://tampermonkey.net/
 // @version      2025-04-02
 // @description  try to take over the world!
